f2ae11074089878568c40959d018da9fab3ca077
joaquimatheus <matheus@marcogabriel.com.br>
2021-12-17 23:55:08 -0300

    add system of points, and resolving code trash

--- script.js
+++ script.js
@@ -25,15 +25,19 @@ perguntas2 = [
     "Tomate", "Ameixa", "Pepino", "Uva" 
 ]
 
-let IMPerguntas = function (id, lista) {
-    this.id = id;
+let IMPerguntas = function (lista) {
     this.lista = lista;
+}
+
+let SISTPontos = function(valor, pontosTemporarios, pontos) {
+    this.valor = valor;
+    this.pontosTemporarios = pontosTemporarios;
+    this.pontos = pontos;
 }
 
 IMPerguntas.prototype.imprimir = function () {
     this.escolher = Math.floor(Math.random() * 3)
     let arrayEscolhido = this.lista[this.escolher]
-    console.log(this.escolher)
 
     if (this.escolher == 0) {
         console.log("Qual o oceano que banha o Brasil?")
@@ -53,28 +57,73 @@ IMPerguntas.prototype.acertar = function () {
     if (this.escolher == 0) {
         if(resposta == 1) {
             console.log("Acertou camarada!!")
+            return cont = true;
         } else {
             console.log("Errou =(")
+            return cont = false;
         }        
     } else if (this.escolher == 1) {
         if(resposta == 0) {
             console.log("Acertou camarada uau!!")
+            return cont = true;
         } else {
             console.log("Voce esta totalmente errado e é isso")
+            return cont = false;
         }
     } else {
         if(resposta == 3)  {
             console.log("Acertou QI mais de 8000")
+            return cont = true;
         } else {
             console.log("Sinceramente não sei como você errou isso.")
+            return cont = false;
         }
     } 
 }
 
-IMPerguntas.prototype.calculadoDePontos = function() {
-
+IMPerguntas.prototype.jogar = function () {
+    this.imprimir()
+    this.acertar()
 }
 
-let jogoDoMilhao = new IMPerguntas(1, [perguntas0, perguntas1, perguntas2])
-jogoDoMilhao.imprimir()
-jogoDoMilhao.acertar()
+SISTPontos.prototype.gerarPontos = function() {
+    this.valor = Math.floor(Math.random() * 1000)
+}
+
+SISTPontos.prototype.addPontosTemp = function(pontos) {
+    this.pontosTemporarios += pontos;
+}
+
+SISTPontos.prototype.addPontos = function() {
+    this.pontos += this.pontosTemporarios;
+    console.log(this.pontos);
+}
+
+SISTPontos.prototype.rodar = function() {
+    this.gerarPontos()
+    this.addPontosTemp(this.valor)
+    this.addPontos()
+}
+
+SISTPontos.prototype.zerarPontos = function() {
+    this.pontosTemporarios = 0;
+    this.pontos = 0;
+}
+
+let jogoDoMilhao = new IMPerguntas([perguntas0, perguntas1, perguntas2])
+jogoDoMilhao.jogar(jogoDoMilhao)
+
+let sistemaPontos = new SISTPontos(null, 0, 0);
+
+while (cont) {
+    console.log("");
+    jogoDoMilhao.jogar();
+
+    if(cont == false) {
+        sistemaPontos.zerarPontos();
+        console.log("Você perdeu todos os pontos!")
+    } else {
+        sistemaPontos.rodar();
+    }
+}
+
